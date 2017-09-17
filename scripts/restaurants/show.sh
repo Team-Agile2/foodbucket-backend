34cda32b87a7974a1b1da20c75553da048764f8c
RobelsiTejada <robelsitejada@gmail.com>
2017-09-17 15:57:41 -0400

    fixing scripts restaurant show v14

--- scripts/restaurants/show.sh
+++ scripts/restaurants/show.sh
@@ -6,13 +6,12 @@ URL_PATH="/businesses"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request GET \
-  --header "Authorization: Bearer NHG0larijXXaQKiCWF1D7zz_vpxcnFDPMoIz-i1tRcxdx3Af18IVCYYLXfQGes0o_0R-2TiXKv3qHYs981_sNJiE3yjYZJAkDGEuqNKmzBpZuaxeaBnjF_PtQWC9WXYx" \
+  # --header "Authorization: Bearer NHG0larijXXaQKiCWF1D7zz_vpxcnFDPMoIz-i1tRcxdx3Af18IVCYYLXfQGes0o_0R-2TiXKv3qHYs981_sNJiE3yjYZJAkDGEuqNKmzBpZuaxeaBnjF_PtQWC9WXYx" \
   --header "Authorization: Token token=$TOKEN"
   --data '{
     "businesses": [
-      {
-        "location": {
-          "zip_code": "'"${ZIP_CODE}"'",
+      { "location": {
+          "zip": "'"${ZIP}"'",
           "latitude": "'"${LATITUDE}"'",
           "longitude": "'"${LONGITUDE}"'",
           "radius": "'8046'"
