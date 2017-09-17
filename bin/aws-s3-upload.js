fa411ff69124de8a02384d2f44705b70c2fddf6f
RobelsiTejada <robelsitejada@gmail.com>
2017-09-16 20:30:18 -0400

    fixing heroku bugs v4 upload s3 enabled

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -1,26 +1,26 @@
-// 'use strict'
-//
-// const mongoose = require('./../app/middleware/mongoose')
-//
-// const Upload = require('./../app/models/upload')
-//
-// const awsUpload = require('./../lib/aws-upload')
-//
-// // create an object to store the values passed in by
-// // the user/command line
-// const file = {
-//   path: process.argv[2],
-//   name: process.argv[3] || 'default'
-// }
-//
-// // pass file to awsUpload and being promise chain
-// awsUpload(file)
-//   .then((s3Response) => {
-//     return Upload.create({
-//       url: s3Response.Location,
-//       title: s3Response.Key
-//     })
-//   })
-//   .then(console.log)
-//   .catch(console.log)
-//   .then(() => mongoose.connection.close())
+'use strict'
+
+const mongoose = require('./../app/middleware/mongoose')
+
+const Upload = require('./../app/models/upload')
+
+const awsUpload = require('./../lib/aws-upload')
+
+// create an object to store the values passed in by
+// the user/command line
+const file = {
+  path: process.argv[2],
+  name: process.argv[3] || 'default'
+}
+
+// pass file to awsUpload and being promise chain
+awsUpload(file)
+  .then((s3Response) => {
+    return Upload.create({
+      url: s3Response.Location,
+      title: s3Response.Key
+    })
+  })
+  .then(console.log)
+  .catch(console.log)
+  .then(() => mongoose.connection.close())
