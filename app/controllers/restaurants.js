8851e957928c848bae463ad47622116ee738a9e1
RobelsiTejada <robelsitejada@gmail.com>
2017-09-17 21:37:28 -0400

    fixing scripts v24 show restaurants fix

--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -6,58 +6,24 @@ const Restaurants = models.restaurants
 const authenticate = require('./concerns/authenticate')
 const setUser = require('./concerns/set-current-user')
 const setModel = require('./concerns/set-mongoose-model')
-// const yelp = require('yelp-fusion')
-// const clientId = 'xlACoXuuSZmb83hJcDxgSg'
-// const clientSecret = 'qiH6mwAcjzmzaW1hZ8uvkD9ESq8JWCCUtmBbz3NWs0cbRZRHyFa7J8r0JjT36Gaz'
-//
-// // gets access token for yelp
-// const client = yelp.client(yelp.accessToken(clientId, clientSecret).then(response => {
-//   console.log(response.jsonBody.access_token)
-// }).catch(e => {
-//   console.log(e)
-// }))
+const yelp = require('yelp-fusion')
+const clientId = 'xlACoXuuSZmb83hJcDxgSg'
+const clientSecret = 'qiH6mwAcjzmzaW1hZ8uvkD9ESq8JWCCUtmBbz3NWs0cbRZRHyFa7J8r0JjT36Gaz'
 
-const _send = require('@tonybadguy/call-me-maybe')
+// gets access token for yelp
 
-class YelpClient {
-  constructor (token) {
-    this.token = token
-  }
+yelp.accessToken(clientId, clientSecret).then(response => {
+  const client = yelp.client(response.jsonBody.access_token)
 
-  search (parameters) {
-    return _send({
-      url: 'https://api.yelp.com/v3/businesses/search',
-      query: parameters,
-      bearerToken: this.token
-    })
-  }
-
-  business (id) {
-    return _send({
-      url: 'https://api.yelp.com/v3/businesses/{id}',
-      urlParams: {
-        id: id
-      },
-      bearerToken: this.token
-    })
-  }
-}
-
-const accessToken = (clientId, clientSecret) => {
-  return _send({
-    url: 'https://api.yelp.com/oauth2/token',
-    method: 'post',
-    urlencodedBody: {
-      grant_type: 'client_credentials',
-      client_id: 'xlACoXuuSZmb83hJcDxgSg',
-      client_secret: 'qiH6mwAcjzmzaW1hZ8uvkD9ESq8JWCCUtmBbz3NWs0cbRZRHyFa7J8r0JjT36Gaz'
-    }
+  client.search({
+    term: 'restaurants',
+    location: 'providence, ri'
+  }).then(response => {
+    console.log(response.jsonBody.businesses[0].name)
   })
-}
-
-const createClient = (token) => {
-  return new YelpClient(token)
-}
+}).catch(e => {
+  console.log(e)
+})
 
 const index = (req, res, next) => {
   Restaurants.find()
@@ -87,13 +53,25 @@ const destroy = (req, res, next) => {
     .catch(next)
 }
 
+const create = (req, res, next) => {
+  const restaurants = Object.assign(req.body.restaurants, {
+    _owner: req.user._id
+  })
+  Restaurants.create(restaurants)
+    .then(restaurants =>
+      res.status(201)
+        .json({
+          restaurants: restaurants.toJSON({ virtuals: true, user: req.user })
+        }))
+    .catch(next)
+}
+
 module.exports = controller({
-  client: createClient,
-  accessToken: accessToken,
   index,
   show,
   update,
-  destroy
+  destroy,
+  create
 }, { before: [
   { method: setUser, only: ['index', 'show'] },
   { method: authenticate, except: ['index', 'show'] },
--- scripts/restaurants/show.sh
+++ scripts/restaurants/show.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
 
-API="${API_ORIGIN:-https://api.yelp.com/v3}"
-URL_PATH="/businesses/search"
-
-curl "${API}${URL_PATH}/${ID}" \
-  --include \
-  --request GET \
+curl \
+  -X GET \
   -H "Authorization: Bearer NHG0larijXXaQKiCWF1D7zz_vpxcnFDPMoIz-i1tRcxdx3Af18IVCYYLXfQGes0o_0R-2TiXKv3qHYs981_sNJiE3yjYZJAkDGEuqNKmzBpZuaxeaBnjF_PtQWC9WXYx" \
-  -H "Content-Type: application/json" \
-  --data '{
-    "search": {
-      "term": "restaurants",
-      "location": {
-      "latitude": "'"${LATITUDE}"'",
-      "longitude": "'"${LONGITUDE}"'",
-      "radius": "'8000'"
-    }
-  }
-}'
+  "https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=41.792875&longitude=-71.414163&radius=8047"
 
 echo
