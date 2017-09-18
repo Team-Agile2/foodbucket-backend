27a3683701b60756d1d0cd6a10cccaba30f7afc2
RobelsiTejada <robelsitejada@gmail.com>
2017-09-17 20:23:09 -0400

    fixing scripts v23

--- scripts/restaurants/show.sh
+++ scripts/restaurants/show.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-API="https://api.yelp.com/v3"
+API="${API_ORIGIN:-https://api.yelp.com/v3}"
 URL_PATH="/businesses/search"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request GET \
-  --header "Authorization: Bearer NHG0larijXXaQKiCWF1D7zz_vpxcnFDPMoIz-i1tRcxdx3Af18IVCYYLXfQGes0o_0R-2TiXKv3qHYs981_sNJiE3yjYZJAkDGEuqNKmzBpZuaxeaBnjF_PtQWC9WXYx" \
-  --header "Authorization: Token token=$TOKEN"
+  -H "Authorization: Bearer NHG0larijXXaQKiCWF1D7zz_vpxcnFDPMoIz-i1tRcxdx3Af18IVCYYLXfQGes0o_0R-2TiXKv3qHYs981_sNJiE3yjYZJAkDGEuqNKmzBpZuaxeaBnjF_PtQWC9WXYx" \
+  -H "Content-Type: application/json" \
   --data '{
     "search": {
+      "term": "restaurants",
+      "location": {
       "latitude": "'"${LATITUDE}"'",
-      "longitude": "'"${LONGITUDE}"'"
-        }
+      "longitude": "'"${LONGITUDE}"'",
+      "radius": "'8000'"
+    }
+  }
 }'
 
 echo
